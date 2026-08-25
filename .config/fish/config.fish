5b311e8be641d7f0429d6f6f014265560ac3e587
Nilsonlinux <nilsonlinux@gmail.com>
2026-08-25 17:06:24 -0300

    ajust config paths

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+set -U fish_greeting
+# Adicione isso ao final do arquivo ~/.config/fish/config.fish
+starship init fish | source
+
+    # Commands to run in interactive sessions can go here
+end
